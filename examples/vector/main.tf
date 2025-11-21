3cf526af8c479cb565e1995737af288f9593d86e
Mark Tinderholt <markti@outlook.com>
2025-11-21 11:26:40 -0800

    update vector module

--- examples/vector/main.tf
+++ examples/vector/main.tf
@@ -1,137 +1,70 @@
 
-locals {
+module "white" {
+  source = "../../modules/vector"
+
+  material  = "minecraft:white_wool"
+  length    = 5
+  direction = "up"
   start_position = {
-    x = -30
-    y = 76
-    z = -230
+    x = 486
+    y = 64
+    z = 142
   }
 }
 
-module "up" {
+module "blue" {
   source = "../../modules/vector"
 
-  count = 1
-
-  start_position = local.start_position
-  material       = "stone"
-  direction      = "up"
-  length         = 10
-
-}
-
-
-module "down_start_position" {
-  source         = "../../modules/position"
-  start_position = local.start_position
-  translate_vector = {
-    x = 0
-    y = -1
-    z = 0
-  }
-}
-
-
-module "down" {
-  source = "../../modules/vector"
-
-  count = 1
-
-  start_position = module.down_start_position.result
-  material       = "stone"
-  direction      = "down"
-  length         = 10
-
-}
-
-
-module "north_start_position" {
-  source         = "../../modules/position"
-  start_position = local.start_position
-  translate_vector = {
-    x = 0
-    y = 0
-    z = -1
-  }
-
-}
-
-module "north" {
-
-  count = 1
-
-  source = "../../modules/vector"
-
-  start_position = module.north_start_position.result
-  material       = "minecraft:oak_planks"
-  direction      = "north"
+  material       = "minecraft:blue_wool"
   length         = 3
-
-}
-
-
-module "south_start_position" {
-  source         = "../../modules/position"
-  start_position = local.start_position
-  translate_vector = {
-    x = 0
-    y = 0
-    z = 1
-  }
-
-}
-
-
-module "south" {
-  source = "../../modules/vector"
-
-  start_position = module.south_start_position.result
-  material       = "minecraft:jungle_planks"
-  direction      = "south"
-  length         = 3
-
-}
-
-module "east_start_position" {
-  source         = "../../modules/position"
-  start_position = local.start_position
-  translate_vector = {
-    x = 1
-    y = 0
-    z = 0
-  }
-
-}
-
-module "east" {
-  source = "../../modules/vector"
-
-  start_position = module.east_start_position.result
-  material       = "minecraft:acacia_planks"
-  direction      = "east"
-  length         = 3
-
-}
-
-
-module "west_start_position" {
-  source         = "../../modules/position"
-  start_position = local.start_position
-  translate_vector = {
+  direction      = "west"
+  start_position = module.white.end_position
+  transform = {
     x = -1
     y = 0
     z = 0
   }
-
 }
 
-module "west" {
+module "red" {
   source = "../../modules/vector"
 
-  count = 1
-
-  start_position = module.west_start_position.result
-  material       = "minecraft:dark_oak_planks"
-  direction      = "west"
+  material       = "minecraft:red_wool"
   length         = 3
+  direction      = "east"
+  start_position = module.white.end_position
+  transform = {
+    x = 1
+    y = 0
+    z = 0
+  }
+}
 
+
+module "green" {
+  source = "../../modules/vector"
+
+  material       = "minecraft:green_wool"
+  length         = 3
+  direction      = "north"
+  start_position = module.white.end_position
+  transform = {
+    x = 0
+    y = 0
+    z = -1
+  }
+}
+
+module "yellow" {
+  source = "../../modules/vector"
+
+  material       = "minecraft:yellow_wool"
+  length         = 3
+  direction      = "south"
+  start_position = module.white.end_position
+  transform = {
+    x = 0
+    y = 0
+    z = 1
+  }
 }
